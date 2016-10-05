886082c301db902fab2f2012e05405ad7fc28b99
Konrad Feldmeier <konrad@brainbot.com>
2016-10-05 15:09:17 +0200

    Add Hashable and Signed class to messages #10

--- rex/messages.py
+++ rex/messages.py
@@ -1,8 +1,79 @@
 import rlp
-from ethereum.utils import address, int256, hash32
+from rlp.sedes import binary
+from ethereum.utils import (address, int256, hash32, sha3, big_endian_to_int)
+
+from secp256k1 import PublicKey, ALL_FLAGS, PrivateKey
+
+# FIXME: use state of the art shared context to avoid initialization overhead
+pubkey = PublicKey(flags=ALL_FLAGS)
+privkey = PrivateKey(flags=ALL_FLAGS)
+c_ecdsa_recover_compact = pubkey.ecdsa_recover
+c_ecdsa_sign_compact = privkey.ecdsa_sign_recoverable
 
 
-class Offer(rlp.Serializable):
+class RLPHashable(rlp.Serializable):
+    # _cached_rlp caches serialized object
+
+    @property
+    def hash(self):
+        return sha3(rlp.encode(self, cache=True))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.hash == other.hash
+
+    def __hash__(self):
+        return big_endian_to_int(self.hash)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        try:
+            h = self.hash
+        except Exception:
+            h = ''
+        return '<%s(%s)>' % (self.__class__.__name__, h.encode('hex'))
+
+
+class SignatureMissingError(Exception):
+    pass
+
+
+class Signed(RLPHashable):
+
+    _sender = ''
+    signature = ''
+    fields = [('signature', binary)]
+
+    # def __init__(self, sender=''):
+    #     assert not sender or isaddress(sender)
+    #     super(Signed, self).__init__(sender=sender)
+
+    def __len__(self):
+        return len(rlp.encode(self))
+
+    @property
+    def _hash_without_signature(self):
+        return sha3(rlp.encode(self, self.exclude(['signature'])))
+
+    def sign(self, privkey):
+        assert self.is_mutable()
+        assert isinstance(privkey, bytes) and len(privkey) == 32
+        self.signature = c_ecdsa_sign_compact(self._hash_without_signature, privkey)
+        self.make_immutable()
+        return self
+
+    @property
+    def sender(self):
+        if not self._sender:
+            if not self.signature:
+                raise SignatureMissingError()
+            pub = c_ecdsa_recover_compact(self._hash_without_signature, self.signature)
+            self._sender = sha3(pub[1:])[-20:]
+        return self._sender
+
+
+class Offer(Signed):
 
     fields = [
         ('ask_token', address),
--- rex/tests/test_messages.py
+++ rex/tests/test_messages.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import pytest
 from ethereum.utils import privtoaddr, sha3
 from rex.messages import Offer
@@ -9,8 +10,26 @@ def assets():
     return assets
 
 
+@pytest.fixture()
+def accounts():
+    Account = namedtuple('Account', 'privatekey address')
+    privkeys = [sha3("account:{}".format(i)) for i in range(2)]
+    accounts = [Account(pk, privtoaddr(pk)) for pk in privkeys]
+    return accounts
+
+
 def test_offer(assets):
     o = Offer(assets[0], 100, assets[1], 110, sha3('offer id'), 10)
     assert isinstance(o, Offer)
     serial = o.serialize(o)
     assert Offer.deserialize(serial) == o
+
+
+def test_hashable(assets):
+    o = Offer(assets[0], 100, assets[1], 110, sha3('offer id'), 10)
+    assert o.hash
+
+
+def test_signed(accounts, assets):
+    o = Offer(assets[0], 100, assets[1], 110, sha3('offer id'), 10)
+    o.sign(accounts[0].privatekey)
