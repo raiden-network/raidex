4933ae23343d2a24ebf7f1b3e5ab9398c7a98ffd
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2016-10-06 17:35:04 +0200

    adding some logic

--- rex/commitment_service.py
+++ rex/commitment_service.py
@@ -1,38 +1,63 @@
+import rlp
+import uint32
+import time
+
+from ethereum.utils import sha3
+from raiden.api import transfer
+
+
 class CommitmentService(object):
 
-    def __init__(self, cs_address):
+    def __init__(self, cs_address, fee):
         self.cs_address = cs_address
-
-    commited_offers = dict()
+        self.commited_offers = dict()
+        self.fee = fee
 
     def transfer_listener():
         # When transfer received trigger method
         # send_back_signed_offer()
         pass
 
-    def maker_commitment_received(commitment):
-        # when commitment is received for a given offer sign it and return to sender
-        pass
+    def maker_commitment_received(self, offer, commitment):
+        # sign and send back commitment_proof = sign(commitment_sig, cs)
+        commitment_proof = sign(commitment.signature, self.cs_address)
+        self.send_back_commitment_proof(commitment_proof)
 
-    def taker_commitment_received(commitment):
+    def taker_commitment_received(self, offer, commitment):
         # if offer timed out return commitment
+        if commitment.timeout < time.time() * 1000:
+            self.return_commitment(commitment)
         # if offer already taken return commitment
-        # when commitment is received matching a given offer sign it and return to sender
-        commited_offers[hashlock] = commitment
+        elif sha3(offer) in self.commited_offers:
+            self.return_commitment(commitment)
+        else:
+            # when commitment is received matching a given offer sign it and return to sender
+            hashlock = sha3(offer)
+            self.commited_offers[hashlock] = commitment
+            # sign and send back commitment proof
+            commitment_proof = sign(commitment.signature, self.cs_address)
+            self.send_back_commitment_proof(commitment_proof)
 
-    def redeem_deposit():
-        pass
+    def return_commitment(self, commitment):
+        # used if unsuccessful commit
+        transfer(commitment.token, commitment.amount, commitment.sender)
 
-    def send_back_signed_offer():
+    def redeem_commitment(self, commitment):
+        # from issue#10
+        fee = int(uint32.max_int / self.fee * commitment.amount + 0.5)
+        redeem_amount = commitment.amount - fee
+        transfer(commitment.token, redeem_amount, commitment.sender)
+
+    def send_back_commitment_proof(self, commitment_proof):
         # sign offer and send it back to maker/taker
         pass
 
-    def process_offer(offer):
+    def process_offer(self, offer):
         # wait for commitment to be received as a raiden transfer
         # When commitment is received, sign offer and send back to signee
         pass
 
-    def swap_executed(message):
+    def swap_executed(self, message):
         # decode message and match with commitment
         # register sender
         # if only one or none notify burn / keep deposits
@@ -40,26 +65,16 @@ class CommitmentService(object):
         # redeem deposits (keep a small fee) and broadcast swap completed
         pass
 
-    def broadcast_swap_completed(order_id):
+    def broadcast_swap_completed(self, order_id):
         # send signed message to broadcast channel stating that swap is complete
         pass
 
 
 class Commitment(object):
 
-    def __init__(self, amount, offer, timeout, commitment_id, taker, maker):
+    def __init__(self, amount, offer, timeout, commitment_id, sender):
         self.amount = amount
         self.offer = offer
         self.timeout = timeout
         self.commitment_id = commitment_id
-
-
-# this might be part of messages.py
-# class Offer(object):
-
-    # def __init__(self, bid_token, bid_amount, ask_token, ask_amount, offer_id):
-        # self.bid_token = bid_token
-        # self.bid_amount = bid_amount
-        # self.ask_token = ask_token
-        # self.ask_amount = ask_amount
-        # self.offer_id = offer_id
+        self.sender = sender
