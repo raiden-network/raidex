7ce10dd9b5daa941a5d862865440fc98f53da453
ezdac <maxdacjs@gmail.com>
2016-11-09 17:03:16 +0100

    add commitment_services and commitment_service_announcements fixtures

--- rex/tests/conftest.py
+++ rex/tests/conftest.py
@@ -6,6 +6,7 @@ from ethereum.utils import privtoaddr, sha3
 
 from rex.messages import Offer
 from rex.utils import milliseconds
+from rex.commitment_service import CommitmentService
 
 
 @pytest.fixture()
@@ -21,6 +22,13 @@ def accounts():
     accounts = [Account(pk, privtoaddr(pk)) for pk in privkeys]
     return accounts
 
+@pytest.fixture()
+def commitment_services():
+    privkeys = [sha3("cs_account:{}".format(i)) for i in range(2)]
+    fee_rates = [random.randrange( 0.01, 0.5) for _ in privkeys] # XXX check range
+    commitment_services = [CommitmentService(pk, fr)
+                           for pk, fr in zip(privkeys, fee_rates)]
+    return commitment_services
 
 @pytest.fixture(params=[10])
 def offers(request, accounts, assets):
@@ -31,11 +39,24 @@ def offers(request, accounts, assets):
     offers = []
     for i in range(request.param):
         maker = accounts[i % 2]
-        offer = Offer(assets[i % 2], random.randint(1, 100),
-                      assets[1 - i % 2], random.randint(1, 100),
+        offer = Offer(assets[i % 2],
+                      random.randint(1, 100),
+                      assets[1 - i % 2],
+                      random.randint(1, 100),
                       sha3('offer {}'.format(i)),
                       milliseconds.time_int() + i * 1000
                       )
         offer.sign(maker.privatekey)
         offers.append(offer)
     return offers
+
+@pytest.fixture()
+def commitment_service_advertisements(commitment_services):
+    commitment_service_advertisements = [
+        CommitmentServiceAdvertisement(
+            cs.address,
+            cs.commitment_asset,
+            cs.fee_rate
+        ) for cs in commitment_services
+    ]
+    return commitment_service_advertisements
