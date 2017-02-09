1d151b2b3948aa3ecc5c5e43f0901956ec7b999a
agatsoh <sohan.agate@gmail.com>
2017-02-09 17:24:44 +0530

    Adding primeng datatables for OrderBook and OrderHistory removing ag-grid

---
+++ webui/ui-new/src/app/components/orderbook-table.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { OrderService } from '../services/order.service';
+import * as util from '../services/util.service';
+import { OrderBook } from '../model/order-book';
+declare var BigNumber: any;
+declare var Web3;
+let web3 = new Web3();
+
+@Component({
+    selector: 'rex-orderbook-table',
+    templateUrl: 'orderbook-table.component.html'
+})
+export class OrderBookTableComponent implements OnInit {
+    public bids: OrderBook[];
+    public asks: OrderBook[];
+    private orderbookSubscription: Subscription;
+
+    constructor(private orderService: OrderService) {}
+
+    public ngOnInit(): void {
+        this.getOrderBook();
+    }
+
+    getOrderBook(): void {
+        this.orderbookSubscription = this.orderService.getOrderBook().subscribe(
+            data => {
+                this.bids = util.preprocessOrderBook(data.order_book.bids);
+                this.asks = util.preprocessOrderBook(data.order_book.asks);
+            }
+        );
+    }
+}
--- webui/ui-new/src/app/model/order-book.ts
+++ webui/ui-new/src/app/model/order-book.ts
@@ -1,4 +1,8 @@
 export class OrderBook {
     public amount: number;
     public price: number;
+    constructor(amount: number, price: number){
+        this.amount = amount;
+        this.price = price;
+    }
 }
