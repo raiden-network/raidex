8d6a6f68d596cda5efa332c3708ee41215967646
Bernd Bohmeier <berndbohmeier@googlemail.com>
2017-03-30 23:39:14 +0200

    deleted another test file and some styling

--- webui/src/app/model/offer.ts
+++ webui/src/app/model/offer.ts
@@ -1,7 +1,7 @@
 export class Offer {
     public amount: string;
     public price: string;
-    constructor(amount?:string, price?:string){
+    constructor(amount?: string, price?: string) {
         this.amount = amount;
         this.price = price;
     }
