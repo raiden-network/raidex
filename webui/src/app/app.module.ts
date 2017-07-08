deac498a24eb8edb7a0445e3083ef7bd8b7eba97
dakingha69 <dongha.kim210@gmail.com>
2017-07-08 14:15:27 +0200

    Upgrade to angular 4

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -29,6 +30,7 @@ import { OffersTableComponent } from './components/offers-table.component';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
         DataTableModule,
